5685f12596e299b889755df36b2a0a55e167e5a1
Blake <Blakem2020@gmail.com>
2024-10-13 17:32:21 +0100

    Added an event listener that shows the add task popup

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,7 +1,24 @@
 class UI {
   constructor() {
     this.addTaskBtn = document.querySelector("#add-todo-button");
+    this.taskPopup = document.querySelector(".task-popup");
+
+    this.init();
   }
+
+  // Adds event listners
+  init() {
+    // Opens the add task popup
+    this.addTaskBtn.addEventListener("click", () => {
+      console.log("hello");
+      this.showTaskPopup();
+    });
+  }
+
+  showTaskPopup() {
+    this.taskPopup.style.display = "block";
+  }
+
   // TODO: add todo button
   // TODO: show all todos
   // TODO: show today todos
