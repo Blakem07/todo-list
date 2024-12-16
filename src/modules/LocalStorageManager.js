5ab595d854862582b7b118e092ed17d810769706
Blakem07 <Blakem2020@gmail.com>
2024-12-16 16:18:26 +0000

    ADDED: a new class called LocalStorageManager

---
+++ src/modules/LocalStorageManager.js
@@ -0,0 +1,40 @@
+class LocalStorageManager {
+    static setItem(key, value) {
+    // Save an item to localStorage  
+        try {
+            const serializedValue = JSON.stringify(value);
+            localStorage.setItem(key, serializedValue);
+        } catch (error) {
+            console.error("Error saving to localStorage", error);
+        }
+    }
+
+    static getItem(key) {
+    // Get an item from localStorage    
+        try {
+            const serializedValue = localStorage.getItem(key);
+            return serializedValue ? JSON.parse(serializedValue) : null; // This reverts the serialzed value back to its original state
+        } catch (error) {
+            console.error("Error retrieving from localStorage", error);
+            return null;
+        }
+    }
+
+    static removeItem(key) {
+    // Remove an item from localStorage    
+        try {
+            localStorage.removeItem(key);
+        } catch (error) {
+            console.error("Error removing from localStorage", error);
+        }
+    }
+
+    static clear() {
+    // Clear all localStorage data    
+        try {
+            localStorage.clear();
+        } catch (error) {
+            console.error("Error clearing localStorage", error);
+        }
+    }
+}
