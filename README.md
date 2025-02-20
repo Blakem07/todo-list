145923d1d92e9c07b48f6b1d51cb4ee6e404eb37
Blakem07 <Blakem2020@gmail.com>
2025-02-20 14:39:12 +0000

    Updated readme and removed commented out code

--- README.md
+++ README.md
@@ -6,15 +6,26 @@ A Todo-list web application using JavaScript, HTML and CSS
 
 Created from a webpack template (https://github.com/Blakem07/webpack-template.git)
 
-## Date Handling with datesfn
+## Key Features
 
-This app uses the [datesfn](https://www.npmjs.com/package/datesfn) library for efficient date manipulation and formatting.
+- Multiple Projects: Create and manage multiple projects to organize your todos.
+- Dynamic Todos: Add todos with various properties like due date, priority, and notes.
+- UI Interactions: View, edit, expand, and delete todos. Priorities are color-coded for easy management.
+- Persistence: Uses localStorage to save and load todos even after the page is refreshed.
+
+## Technologies Used
+
+- JavaScript (ES6+)
+- Webpack
+- localStorage API
+
+The project is modularized to separate the application logic from the UI logic, making the code clean and easy to maintain. I’ve also used date-fns to help with date formatting and manipulation.
 
 ## Installation Instructions
 
 1. Clone this repo:
 
-   ...
+   git clone git@github.com:Blakem07/todo-list.git
 
 2. Install dependencies:
 
--- src/index.js
+++ src/index.js
@@ -16,31 +16,4 @@ window.todoList.projectManager.createProject("Work");
 // Dummy Todos
 window.todoList.loadDummyTodos();
 
-/* 
-window.todoList.addItem("Study for exam");
-window.todoList.updateProject("Study for exam", "Study");
-window.todoList.addItem("Walk the dog");
-window.todoList.updateProject("Walk the dog", "Personal");
-window.todoList.addItem("Clean bedroom");
-window.todoList.updateProject("Clean bedroom", "Personal");
-window.todoList.addItem("Learn webpack");
-window.todoList.updateComplete("Learn webpack");
-window.todoList.updateProject("Learn webpack", "Study");
-window.todoList.addItem("Learn react");
-window.todoList.updateProject("Learn react", "Study");
-window.todoList.addItem("Finish report", "01/01/2099");
-window.todoList.updateProject("Finish report", "Work");
-window.todoList.updateComplete("Finish report");
-window.todoList.addItem("Client meeting", "07/06/2099");
-window.todoList.updateProject("Client meeting", "Work");
-*/
 const ui = new UI(window.todoList);
-
-// ---------- FIX LIST ----------
-/*
-- Todo cards not saving project and date on page refresh
-    projects now appear on refresh [x]
-    dates [x]
-- Create a way to delete Projects
-- Ensure todoCards dont distort when projects are added
-*/
